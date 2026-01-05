c59b7db982fd05bc7a8e4e5bc462a9e6a9c7c538
jaibnaqi <shahzaibnaqvi07@gmail.com>
2026-01-05 12:35:19 -0800

    Describe your changes

--- components/Services/Intro/Intro.tsx
+++ components/Services/Intro/Intro.tsx
@@ -1,13 +1,20 @@
-const Intro = () => {
+type Props = {
+  heading?: string;
+  description?: string;
+};
+
+const Intro: React.FC<Props> = ({
+  heading = "Stunning websites that supercharge your brand.",
+  description = "Don't just be noticed – become unmissable, with a captivating website that stops people in their tracks.",
+}) => {
   return (
     <section className="mx-auto flex max-w-[calc(1400px+15%)] flex-col px-[7.5%] py-[5vw] lg:grid lg:grid-cols-12 2xl:max-w-[1800px+15%]">
       <div className="col-span-12 grid grid-cols-12 items-center gap-4 py-[6em] lg:gap-0">
         <h1 className="text-fs-60 col-span-12 leading-none lg:col-span-6">
-          Stunning websites that supercharge your brand.
+          {heading}
         </h1>
         <p className="col-span-12 text-base lg:col-span-5 lg:col-start-8 lg:text-2xl">
-          Don’t just be noticed – become unmissable, with a captivating website
-          that stops people in their tracks.
+          {description}
         </p>
       </div>
     </section>
